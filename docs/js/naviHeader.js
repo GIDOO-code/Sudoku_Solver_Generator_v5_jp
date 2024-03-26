a9166e8d23f88ef04558782828d851f006431a0f
GIDOO-code <gnp1415eng@gmail.com>
2024-03-26 12:51:03 +0900

    GNPX v5 brta 0326

--- docs/js/naviHeader.js
+++ docs/js/naviHeader.js
@@ -6,6 +6,7 @@ function writeNaviHeader( ){
 			html += '<li><a class="a" href="index_v5.html">Home(v5)</a></li>';
 
 			html += '<li><a href="page2.html">Sudoku Algorithm</a></li>';
+			html += '<li><a href="page2_v5.html">Sudoku Algorithm v5</a></li>';
 		//	html += '<li><a href="page1.html">Element Tech</a></li>';
 			html += '<li><a href="page17.html">Download</a></li>';
 			html += '<li><a href="page19.html">about this</a></li>';
